b29ecc70d705e326ffd5adda2559818d32cba009
Jimmy Brawn <jimmyb@opera.com>
2018-05-13 23:31:16 +0200

    temporary

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	todos := newTodo()
+	fmt.Println(b)
+}
---
+++ todo.go
@@ -0,0 +1,19 @@
+package main
+
+type todos []string
+
+// func newTodo(text ...string) todos {
+// 	t := todos{}
+// 	if text != "safe" {
+
+// 		reader := bufio.NewReader(os.Stdin)
+// 		fmt.Print("What do you want to do? \n")
+// 		todoText, _ := reader.ReadString('\n')
+// 		go newTodo(todoText)
+// 	} else {
+// 		z := append(t, todoText)
+// 		return z
+// 	}
+
+// 	return nil
+// }
